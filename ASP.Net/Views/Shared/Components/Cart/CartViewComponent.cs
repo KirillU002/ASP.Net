bb7f84b34c3faaf67eb2a795ca34922d65e175bb
Кирилл <92627271+KirillU002@users.noreply.github.com>
2022-09-09 15:09:56 +0300

    исправлено кол-во продуктов в хедере у корзины

---
+++ ASP.Net/Views/Shared/Components/Cart/CartViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApplication;
+
+namespace ASP.Net.Views.Shared.ViewComponents.CartViewComponents
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly ICartsRepository cartsRepository;
+
+        public CartViewComponent(ICartsRepository cartsRepository)
+        {
+            this.cartsRepository = cartsRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var cart = cartsRepository.TryGetByUserId(Constatns.UserId);
+            var productCounts = cart?.AmountMonitor ?? 0;
+
+            return View("Cart", productCounts);
+        }
+    }
+}
