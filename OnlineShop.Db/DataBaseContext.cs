e31fd06a2c270309b000e6a5aa8af4a7bdad9648
Kirill <Kirill@DESKTOP-RPQCACJ>
2022-10-23 18:18:40 +0300

    Миграция БД

--- OnlineShop.Db/DataBaseContext.cs
+++ OnlineShop.Db/DataBaseContext.cs
@@ -5,14 +5,15 @@ namespace OnlineShop.Db
 {
     public class DataBaseContext : DbContext
     {
-        public DbSet<Product> Products { get; set; }//доступ к таблицам
+        public DbSet<Product> Products { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public DbSet<FavoriteProduct> FavoriteProducts { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<CompareProduct> CompareProducts { get; set; }
+
         public DataBaseContext(DbContextOptions options) : base(options)
         {
-            Database.EnsureCreated();//создаем бд при 1 обращении
+            Database.Migrate();
         }
     }
 }
